76fd464dd660ed8c4debd1164e6550e1e35f1086
Trojplosnik <stormtrooper3711@mail.ru>
2023-06-27 20:12:51 +0500

    change drow methods

--- src/main/java/gui/GameVisualizer.java
+++ src/main/java/gui/GameVisualizer.java
@@ -12,24 +12,20 @@ import java.awt.Graphics2D;
 import java.awt.geom.AffineTransform;
 
 public class GameVisualizer extends JPanel {
-    private final GameModel robotModel;
+    private final GameModel gameModel;
 
 
     public GameVisualizer(GameModel Model) {
-        robotModel = Model;
+        gameModel = Model;
     }
 
     @Override
     public void paint(Graphics g)
     {
         super.paint(g);
-        Graphics2D g2d = (Graphics2D) g.create();
-        try {
-            drawRobot(g2d, robotModel);
-            drawTarget(g2d, robotModel);
-        } finally {
-            g2d.dispose();
-        }
+        Graphics2D g2d = (Graphics2D)g;
+        drawRobot(g2d, gameModel);
+        drawTarget(g2d, gameModel);
     }
 
     private static void fillOval(Graphics g, int centerX, int centerY, int diam1, int diam2)
@@ -42,30 +38,42 @@ public class GameVisualizer extends JPanel {
         g.drawOval(centerX - diam1 / 2, centerY - diam2 / 2, diam1, diam2);
     }
 
-    private void drawRobot(Graphics2D g, GameModel model)
+    private void drawRobot(Graphics2D graphics, GameModel model)
     {
         int robotCenterX = Calculator.round(model.robotX());
         int robotCenterY = Calculator.round(model.robotY());
         AffineTransform t = AffineTransform.getRotateInstance(model.robotDirection(), robotCenterX, robotCenterY);
-        g.setTransform(t);
-        g.setColor(Color.MAGENTA);
-        fillOval(g, robotCenterX, robotCenterY, 30, 10);
-        g.setColor(Color.BLACK);
-        drawOval(g, robotCenterX, robotCenterY, 30, 10);
-        g.setColor(Color.WHITE);
-        fillOval(g, robotCenterX  + 10, robotCenterY, 5, 5);
-        g.setColor(Color.BLACK);
-        drawOval(g, robotCenterX  + 10, robotCenterY, 5, 5);
+        Graphics2D g = (Graphics2D)graphics.create();
+        try {
+            g.setTransform(t);
+            g.setColor(Color.MAGENTA);
+            fillOval(g, robotCenterX, robotCenterY, 30, 10);
+            g.setColor(Color.BLACK);
+            drawOval(g, robotCenterX, robotCenterY, 30, 10);
+            g.setColor(Color.WHITE);
+            fillOval(g, robotCenterX  + 10, robotCenterY, 5, 5);
+            g.setColor(Color.BLACK);
+            drawOval(g, robotCenterX  + 10, robotCenterY, 5, 5);
+        }
+        finally {
+            g.dispose();
+        }
     }
 
-    private void drawTarget(Graphics2D g, GameModel model)
+    private void drawTarget(Graphics2D graphics, GameModel model)
     {
         AffineTransform t = AffineTransform.getRotateInstance(0, 0, 0);
-        g.setTransform(t);
-        g.setColor(Color.GREEN);
-        fillOval(g, model.targetX(), model.targetY(), 5, 5);
-        g.setColor(Color.BLACK);
-        drawOval(g, model.targetX(), model.targetY(), 5, 5);
+        Graphics2D g = (Graphics2D)graphics.create();
+        try {
+            g.setTransform(t);
+            g.setColor(Color.GREEN);
+            fillOval(g, model.targetX(), model.targetY(), 5, 5);
+            g.setColor(Color.BLACK);
+            drawOval(g, model.targetX(), model.targetY(), 5, 5);
+        }
+        finally {
+            g.dispose();
+        }
     }
 
 
